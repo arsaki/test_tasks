ce96cfa72cbf1728e234e6050b5ed9aa31ee99d1
Arseniy Akimov <a.akimov@aerodisk.ru>
2023-11-30 15:54:19 +0300

    parallel version in process...

--- sber/sbertask.c
+++ sber/sbertask.c
@@ -32,6 +32,8 @@
 #include <linux/list.h>
 #include <linux/mutex.h>
 #include <linux/spinlock.h>
+#include <linux/sched.h>
+#include <linux/rbtree.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Arsenii Akimov <arseniumfrela@bk.ru>");
@@ -57,33 +59,59 @@ struct queue *queue_head = NULL;
 struct queue *queue_tail = NULL;
 DEFINE_MUTEX(read_mutex);
 DEFINE_SPINLOCK(queue_lock);
+DEFINE_SPINLOCK(rb_tree_lock);
+
+static int add_pid(pid_t pid){
+	spin_lock(&rb_tree_lock);
+
+	spin_unlock(&rb_tree_lock);
+	return 0;
+}
+
+static int rm_pid(pid_t pid){
+	spin_lock(&rb_tree_lock);
+
+	spin_unlock(&rb_tree_lock);
+	return 0;
+}
+
+static void *get_queue_from_pid(pid_t pid){
+	spin_lock(&rb_tree_lock);
+
+	spin_unlock(&rb_tree_lock);
+	return NULL;
+}
+
+
 
 static int sbertask_open (struct inode *inode, struct file *file_p)
 {
+
 	try_module_get(THIS_MODULE);
+	if (add_pid(current->pid))
+		pr_info("sbertask: process with pid %u opened device\n");
+	/* Cache create */
+	queue_cache = kmem_cache_create("sbertask_queue", sizeof(struct queue), 0, SLAB_HWCACHE_ALIGN, NULL);
+	if (queue_cache == NULL){
+		pr_err("sbertask: can't create queue cache\n");
+	}
+
+	/* Mutex */
+	mutex_init(&read_mutex);
+	if (!mutex_lock_interruptible(&read_mutex))
 	pr_info("sbertask: device %s opened\n", DEVICE_NAME);
 	return 0;
 };
 
 static  ssize_t sbertask_read (struct file *file_p, char __user *buf, size_t length, loff_t *off_p)
 {		
-	struct queue *old_entry;
-	pr_info("sbertask: read\n");
+	struct queue *old_entry, *queue_head;
 
-	if(!queue_length){
-		pr_info("sbertask: queue is empty\n");
-		if (!mutex_lock_interruptible(&read_mutex)){
-			if(mutex_lock_interruptible(&read_mutex)==-EINTR){
-				pr_info("Interrupt signal caught 2");
-				return 0;
-			}
-			pr_info("Interrupt signal caught 1");
-			return 0;
-		}
-	}
-	if (queue_head == NULL){
-		pr_err("sbertask: queue_head is NULL!!!!");
-		return 0;
+	pr_info("sbertask: process with pid %u read device\n", current->pid);	
+	queue_head = get_queue_from_pid(current->pid);                
+	if(queue_head == NULL){
+		pr_info("sbertask: queue is empty for process with pid %u\n", current->pid);
+		/* lock reading with rw semaphore */
 	}
 	spin_lock(&queue_lock);
 	if(put_user(queue_head->data, buf)){
@@ -92,26 +120,23 @@ static  ssize_t sbertask_read (struct file *file_p, char __user *buf, size_t len
 		return -EINVAL;
 	}
 	pr_info("sbertask: sended char %c\n", queue_head->data);
-
-	if(queue_length > 1){
+	/* time to delete entry */
+	if(queue_head->next != queue_head){
 		old_entry = queue_head;
 		queue_head = list_entry(queue_head->list.next, struct queue, list);
 		list_del(&old_entry->list);
 		kmem_cache_free(queue_cache, old_entry);
-	}	
-	if(queue_length <= 1){
+	else
 		kmem_cache_free(queue_cache, queue_head);
-		queue_head = NULL;
-		queue_tail = NULL;
+		/* qet queue_head in rb_tree to null */
 	}
-	queue_length--;
 	spin_unlock(&queue_lock);
 	return 1;
 };
 
 static	ssize_t sbertask_write (struct file *file_p, const char __user *buf, size_t length, loff_t *off_p)
 {
-	pr_info("sbertask: write\n");
+	pr_info("sbertask: process with pid %u write device\n", current->pid);	
 	if (queue_length >= QUEUE_DEPTH){	
 		pr_info("sbertask: queue full");
 		return 0;
@@ -145,6 +170,8 @@ static	ssize_t sbertask_write (struct file *file_p, const char __user *buf, size
 static int sbertask_release (struct inode *inode, struct file *file_p)
 {
 	module_put(THIS_MODULE);
+        if (rm_pid(current->pid))
+                pr_info("sbertask: process with pid %u closed device\n");
 	mutex_unlock(&read_mutex);
 	pr_info("sbertask: device %s closed\n", DEVICE_NAME);
 	return 0;
@@ -160,36 +187,24 @@ const struct file_operations f_ops = {
 
 static int __init module_start(void)
 {
-	int ret;
+	struct rb_root root = RB_ROOT;
+
 	pr_info("sbertask: mode %s\n", mode_string);
+	
+	/* Register /dev/sbertask */
 	major_number = register_chrdev(0, DEVICE_NAME, &f_ops);
 	if (major_number < 0){
-		pr_err("Can't register device %s", DEVICE_NAME);
-		ret = 2;
-		goto register_error;
+		pr_err("sbertask: can't register device %s", DEVICE_NAME);
+		return 2;
+
 	}
 	pr_info("sbertask: assigned major number %d\n", major_number);
-	queue_cache = kmem_cache_create("sbertask_queue", sizeof(struct queue), 0, SLAB_HWCACHE_ALIGN, NULL);
-	if (queue_cache == NULL){
-		pr_err("sbertask: can't create queue cache\n");
-		ret = 3;
-		goto cache_error;
-	}
-	mutex_init(&read_mutex);
-	if (mutex_lock_interruptible(&read_mutex)){
-		ret = 1;
-		goto mutex_error;
-	} else{
-		pr_info("sbertask: module successfully loaded\n");
-		return 0;
-	}
-mutex_error:
-	kmem_cache_destroy(queue_cache);
-cache_error:
-	unregister_chrdev(major_number, DEVICE_NAME);
-register_error:
-	return ret;		
-	
+
+
+	pr_info("sbertask: module successfully loaded\n");
+	return 0;
+
+
 };
 
 static void __exit module_stop(void)
