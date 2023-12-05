a75fa62d7f6210ee2d68b8b0b52bed74459f9eb3
Arseniy Akimov <a.akimov@aerodisk.ru>
2023-12-05 14:53:21 +0300

    added test util read_and_write

---
+++ sber/read_and_write.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1000
+void main (void){
+	const void *s = "1234567890";
+	void *d;
+	int fd;
+	int res;
+	fd = open("/dev/sbertask", O_CREAT | O_TRUNC | O_RDWR, 0666);
+	res = write(fd, s, strlen(s));
+	d = malloc(BUF_SIZE);
+	res = read(fd, d, BUF_SIZE);
+	close(fd);
+	printf("%s\n", d);
+	free(d);
+}
